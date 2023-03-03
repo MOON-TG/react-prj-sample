0d481dea11b324b55ee4b7167887690e849620ed
MOON-TG <takeyou@kakao.com>
2023-03-03 09:56:32 +0900

    2nd

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -13,12 +13,48 @@ function BasicExample() {
         <Navbar.Toggle aria-controls="basic-navbar-nav" />
         <Navbar.Collapse id="basic-navbar-nav">
           <Nav className="me-auto">
-            <Nav.Link href="/">Home</Nav.Link>
-            <Nav.Link href="#home">Robot</Nav.Link>
-            <Nav.Link href="#home">About</Nav.Link>
-            <Nav.Link href="#home">Team</Nav.Link>
-            <Nav.Link href="#home">Board</Nav.Link>
-            <Nav.Link href="#link">Link</Nav.Link>
+            <Nav.Link
+              href="#hero"
+              className="border-indigo-500 hover:border-b-4 active:border-b-4 "
+            >
+              Home
+            </Nav.Link>
+            <Nav.Link
+              href="#robot"
+              className="border-indigo-500 hover:border-b-4"
+            >
+              Robot
+            </Nav.Link>
+            <Nav.Link
+              href="#about"
+              className="border-indigo-500 hover:border-b-4"
+            >
+              About
+            </Nav.Link>
+            <Nav.Link
+              href="#gallery"
+              className="border-indigo-500 hover:border-b-4"
+            >
+              Gallery
+            </Nav.Link>
+            <Nav.Link
+              href="#contact"
+              className="border-indigo-500 hover:border-b-4"
+            >
+              Contact Us
+            </Nav.Link>
+            <Nav.Link
+              href="#home"
+              className="border-indigo-500 hover:border-b-4"
+            >
+              Team
+            </Nav.Link>
+            <Nav.Link
+              href="#home"
+              className="border-indigo-500 hover:border-b-4"
+            >
+              Board
+            </Nav.Link>
           </Nav>
           <button className="w-16 h-8 text-white bg-blue-600 border-none rounded-md hover:bg-blue-800">
             <Link to="/login">Login</Link>
